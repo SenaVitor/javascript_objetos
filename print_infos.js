e54850c0079935b6570397277e80c65524aa430c
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-04-06 22:49:03 -0300

    Print object attributes

---
+++ print_infos.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "Vitor",
+    idade: 19,
+    cpf: "70889039092",
+    email: "vitor@gmail.com"
+}
+
+console.log(`Meu nome é ${cliente.nome} e tenho ${cliente.idade} anos`);
+console.log(cliente.cpf.substring(0, 3));
