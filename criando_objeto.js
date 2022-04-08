a68517947ac71c93a7dfd43aa820ac814b20daac
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-04-08 17:35:06 -0300

    Create an object and add an attribute

---
+++ criando_objeto.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    nome:"Peter Parker",
+    dataNascimento:"25/05/1989",
+    carteiraIdentidade:"999362-1",
+    email:"peterparker@email.com",
+    telefone:"+552711112222",
+    cidade:"Aracati",
+    estado:"CE"
+ }
+
+ pessoa.cpf = "09878998765";
+
+ console.log(pessoa);
