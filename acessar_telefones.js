15205e975665a4af2bda43039b8cc21b3e30fe9f
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-04-08 17:43:43 -0300

    Print all fones attribute data

---
+++ acessar_telefones.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "Vitor",
+    idade: 19,
+    cpf: 70889039092,
+    email: "vitor@gmail.com",
+    fones: ["85989923453", "88978980234"]
+}
+
+cliente.fones.forEach(fone => console.log(fone));
