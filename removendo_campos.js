2069dbe58c03b12412a443ad2eafa770e7f897b6
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-04-08 17:23:25 -0300

    Remove attributes of an object

--- removendo_campos.js
+++ removendo_campos.js
@@ -11,12 +11,10 @@ const objPersonagem = {
 
 delete objPersonagem.aliado
 delete objPersonagem["status"]
-
 console.log(objPersonagem.aliado) //undefined
 console.log(objPersonagem.status) //undefined
 
 const delProp = delete objPersonagem.aliado //retorna true
 const delPropInexistente = delete objPersonagem["endereco"] //Se tentarmos deletar um campo inexistente o retorno também será true
-
 console.log(delProp) //true
 console.log(delPropInexistente) //true
