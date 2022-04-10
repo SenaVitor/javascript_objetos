e228dc198baeb725437e8dde0fb6333abb5a0c42
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-04-09 22:29:43 -0300

    Use for in loop to print some array attributes

---
+++ puxando_relatorios.js
@@ -0,0 +1,37 @@
+const cliente = {
+    nome: "Mayk",
+    idade: 21,
+    cpf: 70889039092,
+    email: "mayk@gmail.com",
+    fones: ["85989923453", "88978980234"],
+    dependentes: [
+        {
+            nome: "Yan",
+            parentesco: "filho",
+            dataNasc : "22/02/2022"
+        },
+        {
+            nome: "Ana",
+            parentesco: "filha",
+            dataNasc: "04/07/2020"
+        }
+
+    ],
+    saldo: 100,
+    depositar: function(valor){
+        this.saldo += valor
+    }
+}
+
+let relatorio = "";
+
+for(info in cliente){
+    if(typeof cliente[info] === "object" || typeof cliente[info] === "function"){
+        continue;
+    }else{
+        relatorio += `
+        ${info}: ${cliente[info]}
+        `
+    }
+}
+console.log(relatorio);
