8a44e4f2c6267210ab79224b27bf890e997bde14
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-04-12 21:42:55 -0300

    create an object and instantiate it using inheritance

---
+++ cliente_generico.js
@@ -0,0 +1,11 @@
+function Cliente(nome, cpf, email, saldo){
+    this.nome = nome
+    this.cpf = cpf
+    this.email = email
+    this.depositar = function(valor){
+        this.saldo += valor
+    }
+}
+
+const c1 = new Cliente("Vitor", "08997842598", "vitor@gmail.com", 100)
+console.log(c1)
