78893aa184e7e85204acccc619363555b325f615
Vitor Lucas <vitorlucasdesenalima@gmail.com>
2022-04-13 21:50:09 -0300

    Use getOwnPropertyDescriptor to get the properties of an object's attribute

---
+++ propriedades_proprias_objeto.js
@@ -0,0 +1,14 @@
+const catalogo = {
+    "editora": "Casa do Código",
+    "catalogo": [
+    {
+      "id": 50,
+      "titulo": "Primeiros Passos com NodeJS",
+      "autor": "João Rubens",
+      "categoria": "programação",
+      "versoes": ["ebook", "impresso"]
+    }
+   ]
+}
+
+console.log(Object.getOwnPropertyDescriptor(catalogo, "editora"))
